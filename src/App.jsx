79b01c9ef9e972cf0eb0dab259040bc1dff9581e
bhagwanmmehra <bhagwanmehra04@gmail.com>
2025-01-03 14:45:01 +0530

    ok

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import "./global.css";
+
+const KEY = "ec80a3bd";
+export const API_URL = `http://www.omdbapi.com/?apikey=${KEY}`;
+
+function App() {
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [movieDetails, setMovieDetails] = useState(null);
+    const resultCount = movieDetails.length;
+    const activeMovieID = movieDetails?.imdbID;
+
+    // useEffect(function(), [])
+    // when movieDetails is null it is first click
+    //
+    useEffect(
+        function () {
+            async function fetchMovies() {
+                const response = await fetch(`${API_URL}&s=${query}`);
+                const data = await response.json();
+                setMovies(data.Search || []);
+            }
+            if (query.length >= 3) fetchMovies();
+            else {
+                setMovies([]);
+                setMovieDetails(null);
+            }
+        },
+        [query]
+    );
+
+    function handleChangeSearchQuery(e) {
+        setQuery(e.target.value);
+    }
+
+    async function handleMovieCardClick(imdbID) {
+        if (movieDetails?.imdbID === imdbID) {
+            setMovieDetails(null);
+            // here optional chaining is used
+            return;
+        }
+
+        const response = await fetch(`${API_URL}&i=${imdbID}`);
+        const data = await response.json();
+        setMovieDetails(data);
+    }
+
+    return (
+        <div>
+            <Nav
+                handleChange={handleChangeSearchQuery}
+                query={query}
+                resultCount={resultCount}
+            />
+            <Main
+                movies={movies}
+                handleMovieCardClick={handleMovieCardClick}
+                movieDetails={movieDetails}
+                activeMovieID={activeMovieID}
+            />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Box2.jsx
@@ -0,0 +1,16 @@
+import styles from "../styles/Box2.module.css";
+import MovieDetails from "./MovieDetails";
+// import WatchedMovieList from "./WatchedMovieList";
+// import WatchSummery from "./WatchSummery";
+
+function Box2({ movieDetails }) {
+    return (
+        <div className={styles.box2}>
+            {/* <WatchSummery />
+            <WatchedMovieList /> */}
+            {movieDetails && <MovieDetails movieDetails={movieDetails} />}
+        </div>
+    );
+}
+
+export default Box2;
